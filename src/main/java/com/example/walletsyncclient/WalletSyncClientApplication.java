af0fcfddbdbea70c420607b0879f28b95f632a14
euncheol-kim <klmeuncheol@kakao.com>
2023-08-19 18:51:21 +0900

    init : docker container 구성환경 설정

---
+++ src/main/java/com/example/walletsyncclient/WalletSyncClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.walletsyncclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletSyncClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WalletSyncClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/walletsyncclient/WalletSyncClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.walletsyncclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletSyncClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
